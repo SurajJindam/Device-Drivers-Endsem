653dbadbbace8154429de226349a790578dd683e
Suraj Jindam <57936889+SurajJindam@users.noreply.github.com>
2021-04-15 11:58:54 +0530

    Add files via upload

---
+++ 2/a/integer_param.c
@@ -0,0 +1,11 @@
+#include<linux/module.h>
+static int count =5;
+int init_module(void){
+	printk("The count integer parameter = %d\n",count);
+	return 0;
+}
+void cleanup_module(void){
+	printk("Module removed");
+}
+module_param(count,int,0);
+MODULE_LICENSE("GPL");
